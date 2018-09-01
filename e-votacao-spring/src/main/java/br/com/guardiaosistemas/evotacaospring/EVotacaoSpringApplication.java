47dfbcfef3b512f0658ff214ed68e7a33008994c
Pablo Lima <pabloguardiao@gmail.com>
2018-09-01 09:19:11 -0300

    Commit inicial e-votacao usando spring

---
+++ e-votacao-spring/src/main/java/br/com/guardiaosistemas/evotacaospring/EVotacaoSpringApplication.java
@@ -0,0 +1,12 @@
+package br.com.guardiaosistemas.evotacaospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVotacaoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVotacaoSpringApplication.class, args);
+	}
+}
---
+++ e-votacao-spring/src/test/java/br/com/guardiaosistemas/evotacaospring/EVotacaoSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.guardiaosistemas.evotacaospring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EVotacaoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
